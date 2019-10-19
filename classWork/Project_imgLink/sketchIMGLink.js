ad8b64d4fa5ff3057a687ec8709dfb578042c6d7
OguchiIKE <44120061+OguchiIKE@users.noreply.github.com>
2019-10-19 01:51:25 -0500

    Update sketchIMGLink.js

--- classWork/Project_imgLink/sketchIMGLink.js
+++ classWork/Project_imgLink/sketchIMGLink.js
@@ -2,7 +2,7 @@
 var img;
 
 function preload(){
-    img = loadImage("https://github.com/OguchiIKE/Anyaele_Nnamdi_ART2210/blob/master/classWork/Project_imgLink/Horizon.PNG")
+    img = loadImage("/Horizon.PNG")
    
 }
 
